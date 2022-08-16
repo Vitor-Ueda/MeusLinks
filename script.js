88e11a3d6f9af1bff918e655309842e76b2975ac
Vitor Ueda <vitorkazuma@gmail.com>
2022-08-16 12:30:41 -0300

    First Commit my links

---
+++ script.js
@@ -0,0 +1,32 @@
+document.body.setAttribute('class', 'undefined');
+
+let buttonDarkPink = document.querySelector('.btn-dark-pink');
+let buttonDarkBlue = document.querySelector('.btn-dark-blue');
+let buttonRose     = document.querySelector('.btn-rose');
+let buttonWhite   = document.querySelector('.btn-white');
+
+buttonDarkPink.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'dark-pink');
+    }
+});
+
+buttonDarkBlue.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'dark-blue');
+    }
+});
+
+buttonRose.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'rose');
+    }
+});
+
+buttonWhite.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'undefined');
+    }
+});
+
+
